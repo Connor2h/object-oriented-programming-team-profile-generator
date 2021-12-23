5050079f560f533ad9d4d6d79d7ee06f0d54bed2
Connor Hollis <connorhollis3d@gmail.com>
2021-12-23 15:55:06 -0700

    added Manager Class that extends Employee/ added test cases that pass after first failing

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,17 @@
+const Manager = require("../lib/Manager");
 
-//creates manager object
-test('creates a Manager object', () =>{
-    const manager = new Manager('Dave');
+//creates manager object with office number
+test('creates an Manager object with office number', () =>{
+    const officeNumber = "1234";
+    const manager = new Manager('Dave', "1", "dave@gmail.com", officeNumber);
 
-    expect(manager.name).toBe('Dave');
-    expect(manager.id).toEqual(expect.any(Number));
-    expect(manager.email).toBe('dave@gmail.com');
+    expect(manager.officeNumber).toBe("1234");
 
 })
+
+//gets manager's role
+test("gets manager's role", () => {
+    const manager = new Manager('Dave', "1", "dave@gmail.com");
+
+    expect(manager.getRole()).toBe('Manager');
+});
