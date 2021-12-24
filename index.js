2b639f4fcaecae3f40cc3fabae20a5256a3b385e
Connor Hollis <connorhollis3d@gmail.com>
2021-12-23 20:41:18 -0700

    pushed manger data into array for later use/ created new function to ask and store team data

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+
+const employeeDatabase = [];
 
 
 const promptUser = () =>{
@@ -28,11 +31,64 @@ const promptUser = () =>{
                     return false;
                 }
             }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "Please enter your team Manager's email. (Required)",
+            validate: managerEmail => {
+                if(managerEmail){
+                    return true
+                }else{
+                    console.log("Please enter your team Manager's email!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerOfficeNum',
+            message: "Please enter your team Manager's office number. (Required)",
+            validate: managerOfficeNum => {
+                if(managerOfficeNum){
+                    return true
+                }else{
+                    console.log("Please enter your team Manager's office number!");
+                    return false;
+                }
+            }
         }
     ]).then(managerAnswers => {
-        console.log(managerAnswers);
+
+        let manager = new Manager(managerAnswers.managerName, managerAnswers.managerID, managerAnswers.managerEmail, managerAnswers.managerOfficeNum);
+        employeeDatabase.push(manager);
+
     })
 
 }
 
-promptUser();
+const promptTeam = () =>{
+    return inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'options',
+            message: 'What would you like to do next? (Check only one)',
+            choices: ['Add an Engineer', 'Add an Intern', 'Finish building my team']
+        },
+    ])
+}
+
+promptUser()
+    .then(promptTeam)
+    .then(optionSelect =>{
+        //check which option they choose
+        if(optionSelect.options[0] === "Add an Engineer"){
+            console.log("Engineer added");
+        }
+        else if(optionSelect.options[0] === "Add an Intern"){
+            console.log("Intern added");
+        }
+        else{
+            console.log("finish building my team");
+        }
+    })
